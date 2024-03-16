8b704684d6eb826603961e2057c03ce87fbb2d4c
siyaphe <siyapdje@yahoo.fr>
2024-03-16 12:27:09 -0400

    objet literal first commit

--- app.js
+++ app.js
@@ -1,60 +1,31 @@
-//DATATYPE
-// String, Numbers, Boolean, null, undefined
+//OBJECT LITERAL-------------
+const person = {
+    firstName: 'Henri',
+    lastName: 'Doe',
+    age: 20,
+    hobbies: ['music', 'movies', 'Soccer'],
+    address: {
+        street: '50 main st',
+        city: 'Monroeville',
+        state: 'PA'
+    }
+}
 
-// const name = 'Henri';
-// const age = 25;
+// console.log(person);
+// console.log(person.firstName);
+// console.log(person.firstName, person.lastName);
 
-//concatenation
-// console.log('My name is name and I am age')
-// console.log('My name is ' + name + ' and I am '+ age);
-//Template String
-// // console.log(`My name is ${name} and I am ${age}`);
-// const hello = console.log(`My name is ${name} and I am ${age}`);
-// console.log(hello);
+// console.log(person.hobbies[1]);
+// console.log(person.address.city);
 
-//length properties
-// const s = 'Hello Malia!';
-// // console.log(s.length);
-// // console.log(s.toUpperCase());
-// // onsole.log(s.toLowerCase());
-// // console.log(s.substring(0, 5).toUpperCase());
+//to create variable
 
-// console.log(s.split(''));
+// const { firstName, lastName } = person;
+// console.log(firstName, lastName);
 
-// const it = 'technology, computers, it, code';
-// console.log(it.split(', '));
+const { firstName, lastName, address: {city}} = person;
 
-//Arrays - variable that hold multiple values
-
-// const numbers = new Array(1,2,3,4,5,6,7,8);
-// console.log(numbers);
-
-const fruits = ['apple', 'banana', 'orange', 10, true];
-
-// console.log(fruits[3]);
-fruits[3] = 'paneapple'
-// console.log(fruits[3]);
-// console.log(fruits);
-
-//PUSH -----if not knowing how many values in your Array---push method
-fruits.push('mangoes');
-//UNSHIFT---------add to the beginning -- unshift
-fruits.unshift('Strawberry');
-//pop ------to take the last one off
-fruits.pop();
-//check what is in Array
-console.log(Array.isArray(fruits));
-// to get the index of a certain values
-console.log(fruits.indexOf('orange'));
-
-
-console.log(fruits);
-
-
-
-
-
-
-
+person.email = 'henri@gmail.com'
+console.log(person);
 
 
