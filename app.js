e2e751600489dbc098a1943fe235c0b78c6f4ae4
siyaphe <siyapdje@yahoo.fr>
2024-03-16 12:43:35 -0400

    todos commit

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-//OBJECT LITERAL-------------
-const person = {
-    firstName: 'Henri',
-    lastName: 'Doe',
-    age: 20,
-    hobbies: ['music', 'movies', 'Soccer'],
-    address: {
-        street: '50 main st',
-        city: 'Monroeville',
-        state: 'PA'
+const todos = [
+    {
+        id: 1,
+        text: 'take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with Jordan',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist Appt',
+        isCompleted: false
     }
-}
+];
 
-// console.log(person);
-// console.log(person.firstName);
-// console.log(person.firstName, person.lastName);
-
-// console.log(person.hobbies[1]);
-// console.log(person.address.city);
-
-//to create variable
-
-// const { firstName, lastName } = person;
-// console.log(firstName, lastName);
-
-const { firstName, lastName, address: {city}} = person;
-
-person.email = 'henri@gmail.com'
-console.log(person);
+console.log(todos[1].text);
 
 
