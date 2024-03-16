9ee7ff38bf9a71a2b79bf97ca0449c397e6f7fd6
siyaphe <siyapdje@yahoo.fr>
2024-03-16 10:31:23 -0400

    assign variable using let and const

---
+++ app.js
@@ -0,0 +1,19 @@
+// console.log('This Henri');
+// console.error('This is an error');
+// console.warn('This is a Warning');
+
+// let, const ------------ASSIGN VARIABLE
+// let --------------you can reasign variable
+let age = 30;
+age = 31;
+console.log(age);
+
+let score;
+score = 10;
+console.log(score);
+
+//const---------------you can not change the value
+const scores = 15;
+console.log(scores);
+
+
