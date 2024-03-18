3ac67a69b940c59a7222dcb5691fd3a8276a461d
siyaphe <siyapdje@yahoo.fr>
2024-03-17 21:30:08 -0400

    Third commit

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 //ELEMENT SELECTORS
 
 // Single Element selectors
-console.log(document.getElementById('my-form'));
+// console.log(document.getElementById('my-form'));
+//     // const form = document.getElementById('my-form');
+//     // console.log(form);
+// console.log(document.querySelector('.container'));
+
+// Multiple Element Selector
+// // console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByTagName('li'));
+
+
+// MANIPULATION THE DOM
+const ul = document.querySelector('.items');
+// ul.remove();
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'malia';
+ul.children[1].innerHTML = 'Medhi';
+ul.lastElementChild.innerHTML = '<h1>Pieri</h1>';
+
+const button = document.querySelector('.button');
+button.style.background = 'red';
+
